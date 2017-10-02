c6ec85e8a03f3f0445b713261132dafb914c1475
Dominik Stucki <dominik.stucki@haag-streit.com>
2017-10-02 18:24:26 +0200

    improve notes.zsh

--- .zsh/notes.zsh
+++ .zsh/notes.zsh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-# Source: https://dev.to/ricardomol/note-taking-from-the-command-line-156
 notes() {
   if [ ! -z "$1" ]; then
-    # Using the "$@" here will take all parameters passed into
-    # this function so we can place everything into our file.
-    echo "$@" >> "$HOME/notes.md"
+    echo $(date +"%d%m%Y-%H%M%S") $@  >> $HOME/notes.md
   else
-    # If no arguments were passed we will take stdout and place
-    # it into our notes instead.
-    cat - >> "$HOME/notes.md"
+    echo $(date +"%d%m%Y-%H%M%S") "$(cat)"  >> $HOME/notes.md
   fi
 }
